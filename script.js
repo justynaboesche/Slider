c40d2c89f442c4694b1d8664314db8cd7cbbc94a
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-14 17:06:11 +0100

    2 functions changeImage & handleCarousel created

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Nic tu nie ma 😱')
+const sliderBox = document.querySelector('.slider-box');
+const leftBtn = document.querySelector('.btn-left');
+const rightBtn = document.querySelector('.btn-right');
+const carouselImages = document.querySelectorAll('.slider-img');
+const carouselWidth = 800;
+const carouselSpeed = 3000;
+
+let index = 0;
+
+const handleCarousel = () => {
+	index++;
+	changeImage();
+};
+
+let startCarousel = setInterval(handleCarousel, carouselSpeed);
+
+const changeImage = () => {
+	if (index > carouselImages.length - 1) {
+		index = 0;
+	} else if (index < 0) {
+		index = carouselImages.lenght - 1;
+	}
+
+	sliderBox.style.transform = `translateX(${- index * carouselWidth}px)`;
+};
